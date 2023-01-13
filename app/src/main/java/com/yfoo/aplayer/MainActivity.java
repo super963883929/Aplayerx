4cbfa183b37cd13a74f6b38c096ce3abad4a2bfc
super963883929 <73236105+super963883929@users.noreply.github.com>
2023-01-13 18:18:46 +0800

    Update MainActivity.java

--- app/src/main/java/com/yfoo/aplayer/MainActivity.java
+++ app/src/main/java/com/yfoo/aplayer/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.tools.aplayer.APlayerActivity;
-
+import com.tools.aplayer.playCallback.OnPlayerCallBack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +21,22 @@ public class MainActivity extends AppCompatActivity {
         String url = "http://vodkgeyttp8.vod.126.net/cloudmusic/IWYjYDQ0ZDExIDMxJDAgIg==/mv/303142/911ffb38fa29e85ce81441f921124e5c.mp4?wsSecret=fb9f1996c735059d75940b6c3c76039d&wsTime=1673410670";
         editText.setText("http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4"); //"http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4"
        // aplayer = new Aplayer("",this,"加载中...");
+         APlayerActivity.setOnPlayerCallBack(new OnPlayerCallBack() {
+            @Override
+            public void onFinish() {
+                Toast.makeText(MainActivity.this, "播放完成", Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onPause() {
+                Toast.makeText(MainActivity.this, "播放暂停", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStart() {
+                Toast.makeText(MainActivity.this, "播放开始", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void playVideo(View view){
